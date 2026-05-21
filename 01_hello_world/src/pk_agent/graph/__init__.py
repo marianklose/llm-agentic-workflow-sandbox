caa0b2f7092c01e3e5b869fec526f5939325ae07
Marian Klose <marian.klose@fu-berlin.de>
2026-05-21 12:23:56 +0200

    Init the graph package

---
+++ 01_hello_world/src/pk_agent/graph/__init__.py
@@ -0,0 +1,6 @@
+"""
+LangGraph workflow construction.
+A LangGraph is basically a flowchart with decision points.
+We have the different nodes (= functions) and the 
+edges between them (= control flow, i.e. which node runs after which).
+"""
