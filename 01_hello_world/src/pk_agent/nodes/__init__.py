1eff3a4d978c5748aaae10a27880bd7fbd6115fc
Marian Klose <marian.klose@fu-berlin.de>
2026-05-21 12:16:57 +0200

    Inite nodes package

---
+++ 01_hello_world/src/pk_agent/nodes/__init__.py
@@ -0,0 +1,6 @@
+"""
+Workflow nodes — the units of work in the LangGraph graph. 
+Each node is basically a function that takes some input,
+does something (e.g. calls an LLM), and returns some output.
+In our case, the nodes take the sate as input and return a dict of updates to the state.
+"""
