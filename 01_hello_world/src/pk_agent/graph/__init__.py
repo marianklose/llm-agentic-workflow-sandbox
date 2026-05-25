299d11db29455980b961193b7a36475d1b900ea4
Marian Klose <marian.klose@fu-berlin.de>
2026-05-25 09:29:39 +0200

    Update comments

--- 01_hello_world/src/pk_agent/graph/__init__.py
+++ 01_hello_world/src/pk_agent/graph/__init__.py
@@ -1,6 +1,5 @@
 """
 LangGraph workflow construction.
 A LangGraph is basically a flowchart with decision points.
-We have the different nodes (= functions) and the 
-edges between them (= control flow, i.e. which node runs after which).
+We have the different nodes (= functions) and the edges between them (= control flow).
 """
